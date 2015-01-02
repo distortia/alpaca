b028c9d9847b30ec0e0a1ae4d770e3b481a3ea1f
jdog1994 <jesusdiedtosaveusfromoursins@gmail.com>
2015-01-01 20:00:26 -0500

    Footer Sizing

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
     <div class="container">
-      <div class="ten columns">
+      <div class="nine columns">
         <p id="date"><p>
       </div>
-      <div id="nav" class="two columns">
+      <div id="nav" class="three columns">
         <ul>
          <li><a href="#"><i id="icon" class="fa fa-twitter"></i></a></li>
          <li><a href="#"><i id="icon" class="fa fa-facebook"></i></a></li>
