1d4fa7a36fd5852954c40634374136601155a958
distortia <nickstalter@gmail.com>
2015-01-05 22:38:50 -0500

    fixed navbar link for home from index to /, which is document root

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
  <div class="container">
 	<nav id="nav" class="ten columns">
 		<ul class="clearfix">
-			<li><a class="button" href="index.php">Home</a></li>
+			<li><a class="button" href="/">Home</a></li>
 			<li><a class="button" href="download.php">Download</a></li>
 			<li><a class="button" href="about.php">About</a></li>
 			<li><a class="button" href="contact.php">Contact</a></li>
