8fda23dd6c1266486e2becbbcdb54c4b9001b12f
dpappa <darrell.pappa@pointstart.us>
2015-01-16 20:53:51 -0500

    added jquery for signup button

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ window.onload = function getDate() {
     var n = "Copyright <i class=\"fa fa-copyright\" style=\"font-size: 15px;\"></i> " + d.getFullYear() + " Alpaca Chat";
     document.getElementById("date").innerHTML = n;
 }
+
+//Signup / Login script
+//<div class="sixteen columns"> <div class="four columns offset-by-six"> <p>Login Stuff goes here!</p> </div> </div>
+$("#signin").click(function () {
+  $("#nav").append('<p> Please work </p>');
+});
