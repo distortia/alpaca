e9974ad7ee291f777e86faf48c051673a7beef78
Nick <nickstalter@gmail.com>
2015-01-22 19:15:01 -0500

    fixed the port issue <3 darrell

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 var moment = require('moment');
 
 server.listen(port, function () {
